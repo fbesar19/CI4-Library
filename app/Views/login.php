45b018934a84ed7385c66c29cc829236faec973e
fbesar19 <fbesar.id@gmail.com>
2021-03-21 03:03:22 +0700

    Layout Awal

---
+++ app/Views/login.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?= base_url(); ?>/css/library.css">
+
+    <title>Library - Login</title>
+    <style>
+
+    </style>
+</head>
+
+<body class="bg-login">
+    <div class="container login-container">
+        <div class="row">
+            <div class="col-md-7">
+                <h1 class="mt-4">Libary</h1>
+                <hr class="divider">
+                <h3>Aplikasi peminjaman buku perpustakaan untuk sekolah</h3>
+            </div>
+            <div class="col-md-5 p-4 bg-white shadow-sm rounded text-center">
+                <form action="">
+                    <div class="input-group input-group-lg mb-3">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg    ">
+                    </div>
+                    <div class="input-group input-group-lg mb-5">
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+                    </div>
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-lg" type="button">Button</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
